2dae08962cb4025084d17f9240c251a1f6c3c394
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-08-17 12:45:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,51 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int minCost(vector<int>& height) {
         // Code here
-        int n = height.size();
-        vector<int> dp(n, -1);
+        vector<int> dp(height.size(), -1);
         dp[0] = 0;
-        for(int i = 1; i < n; i++){
-            int jump_one = dp[i - 1] + abs(height[i] - height[i - 1]);
-            int jump_two = INT_MAX;
-            if(i > 1){
-                jump_two = dp[i - 2] + abs(height[i] - height[i - 2]);
-            }
-            dp[i] = min(jump_one, jump_two);
+        dp[1] = abs(height[1] - height[0]);
+        
+        for(int i = 2; i < height.size(); i++) {
+            int jump1step = dp[i - 1] + abs(height[i] - height[i - 1]);
+            int jump2step = dp[i - 2] + abs(height[i] - height[i - 2]);
+            dp[i] = min(jump1step, jump2step);
         }
         
-        return dp[n - 1];
+        return dp[height.size()- 1];
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string str;
-    getline(cin, str);
-    int t = stoi(str);
-    while (t--) {
-        getline(cin, str);
-        stringstream ss(str);
-        vector<int> arr;
-        int num;
-        while (ss >> num) {
-            arr.push_back(num);
-        }
-        Solution ob;
-        cout << ob.minCost(arr) << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
