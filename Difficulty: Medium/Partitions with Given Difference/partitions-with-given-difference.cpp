87d629ed12e3648d9aec06c135bc5ea81fcacd2a
04Htrap <115011558+04Htrap@users.noreply.github.com>
2025-08-23 23:48:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int returnWays(int idx, vector<int>& arr, int target, vector<vector<int>>& dp) {
+        if(idx == 0) {
+            if(target == 0 && arr[0] == 0) return 2;  // take or not take
+            if(target == 0 || target == arr[0]) return 1;
+            return 0;
+        }
+        
+        if(dp[idx][target] != -1) return dp[idx][target];
+        
+        int not_take = returnWays(idx - 1, arr, target, dp);
+        int take = 0;
+        if(arr[idx] <= target) {
+            take = returnWays(idx - 1, arr, target - arr[idx], dp);
+        }
+        
+        return dp[idx][target] = take + not_take;
+    }
+
+    int countPartitions(vector<int>& arr, int d) {
+        int n = arr.size();
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+
+        // Partition not possible
+        if((sum + d) % 2 != 0 || sum < d) return 0;
+
+        int target = (sum + d) / 2;
+
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        return returnWays(n-1, arr, target, dp);
+    }
+};
