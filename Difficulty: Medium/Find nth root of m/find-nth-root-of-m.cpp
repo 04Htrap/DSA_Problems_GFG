258d8c09e34f6535bea8c860b06e0d5c7345ac25
04Htrap <115011558+04Htrap@users.noreply.github.com>
2026-02-01 16:10:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
@@ -1,24 +1,31 @@
 class Solution {
   public:
-    int checkPow(int mid, int n, int m) {
-       long long ans = 1;
-       for(int i = 0; i < n; i++){
-           ans = ans * mid;
-           if(ans > m) return 2;
-       }
-       if(ans == m) return 1;
-       else return 0;
+  
+    int power(int base, int expo) {
+        int res = 1;
+        
+        for(int i = 0; i < expo; i++)
+            res *= base;
+        return res;
     }
     int nthRoot(int n, int m) {
         // Code here
+        if(m == 0) return 0;
+        if(n == 1) return m;
+        
         int low = 0;
         int high = m;
+        
         while(low <= high) {
-            int mid = low + (high - low) /2;
-            int midN = checkPow(mid, n, m);
-            if(midN == 1) return mid;
-            else if(midN == 0) low = mid + 1;
-            else high = mid - 1;
+            int mid = low + (high - low) / 2;
+            
+            int val = power(mid, n);
+            if(val == m)
+                return mid;
+            else if(val > m)
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
         return -1;
     }
